42a799c9c0822b569c1a2dfdf67aac829caef4a2
Shafkat170 <hossainkmshafkat@gmail.com>
2025-08-05 23:36:16 +0600

    Update pizzahut.sql

--- pizzahut.sql
+++ pizzahut.sql
@@ -1,3 +1,35 @@
+-- creating The Database
+CREATE DATABASE pizza_hut;
+USE pizza_hut;
+CREATE TABLE orders (
+    order_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    order_time TIME NOT NULL,
+    PRIMARY KEY (order_id)
+);
+CREATE TABLE order_details (
+    order_details_id INT NOT NULL,
+    order_id INT NOT NULL,
+    pizza_id TEXT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_details_id)
+);
+CREATE TABLE pizza_types (
+    pizza_type_id TEXT PRIMARY KEY,
+    name TEXT,
+    category TEXT,
+    ingredients TEXT
+);
+CREATE TABLE pizzas (
+    pizza_id TEXT PRIMARY KEY,
+    pizza_type_id TEXT,
+    size TEXT,
+    price DOUBLE,
+    FOREIGN KEY (pizza_type_id) REFERENCES pizza_types(pizza_type_id)
+);
+
+
+
 -- Retrieve the total number of orders placed.
 
 select count(distinct order_id)as total_orders from orders;-- 21350
