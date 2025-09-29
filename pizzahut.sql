726483ca7c1f4a2b413cf79adecaef9f2bee9c46
K.M Shafkat Hossain <hossainkmshafkat@gmail.com>
2025-09-29 13:47:42 +0600

    Update pizzahut.sql

--- pizzahut.sql
+++ pizzahut.sql
@@ -7,6 +7,14 @@ CREATE TABLE orders (
     order_time TIME NOT NULL,
     PRIMARY KEY (order_id)
 );
+CREATE TABLE order_details (
+    order_details_id INT NOT NULL,
+    order_id INT NOT NULL,
+    pizza_id TEXT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_details_id)
+);
+
 
 -- Retrieve the total number of orders placed.
 
