264f745080d49160decb88c7e386cf0e5209603a
K.M Shafkat Hossain <hossainkmshafkat@gmail.com>
2025-09-29 13:47:11 +0600

    Update pizzahut.sql

--- pizzahut.sql
+++ pizzahut.sql
@@ -1,3 +1,13 @@
+-- Creating the Database
+CREATE DATABASE pizza_hut;
+USE pizza_hut;
+CREATE TABLE orders (
+    order_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    order_time TIME NOT NULL,
+    PRIMARY KEY (order_id)
+);
+
 -- Retrieve the total number of orders placed.
 
 select count(distinct order_id)as total_orders from orders;-- 21350
